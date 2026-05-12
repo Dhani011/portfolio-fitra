25cb2b05836560946b8e512b24595dfe43ddf592
Fitra Ramadhani <91363029+Dhani011@users.noreply.github.com>
2026-05-12 22:01:57 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Navbar Control
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    navbar.style.transform = "translateY(-500px)";
+}
+
+// Typewriter Effect
+const texts = [
+    "Teleco Engineer",
+    "RF Specialist",
+    "Antenna Designer"
+];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let characterIndex = 0;
+
+function typeWriter() {
+    if (characterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(eraseText, 2000);
+    }
+}
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        characterIndex = 0;
+        setTimeout(typeWriter, 500);
+    }
+}
+
+
+// Initialize
+window.addEventListener('load', () => {
+    // Delay untuk sinkronisasi dengan animasi AOS (900ms delay)
+    setTimeout(typeWriter, 1200);
+});
